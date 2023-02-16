b5d0550a717908d29877bd34d1bf6dd368a9fa27
Viachaslau Kviat <zubr1024@mail.ru>
2023-02-16 23:29:02 +0300

    refactor: add typescript to loader component

---
+++ src/components/controller/interfaces.ts
@@ -0,0 +1,40 @@
+export type Source = {
+  category: string;
+  country: string;
+  description: string;
+  id: string;
+  language: string;
+  name: string;
+  url: string;
+};
+
+export interface ISources {
+  status: string;
+  sources?: Source[];
+}
+
+export type Article = {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  source: {
+    id: string;
+    name: string;
+  };
+  title: string;
+  url: string;
+  urlToImage: string;
+};
+
+export interface IArticles {
+  articles: Article[];
+  status: string;
+  totalResults: number;
+}
+
+export type CallbackType<T> = (data?: T) => void;
+
+export type Options = {
+  apiKey?: string;
+};
