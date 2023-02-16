0546aa9a8defdad108312bd03e489a59f5689af0
Viachaslau Kviat <zubr1024@mail.ru>
2023-02-16 23:26:32 +0300

    feat: add typescript and eslint

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '32331036c43e446c82473aef808bb69f', 
         });
     }
 }
